7b4078a464777db59b2a247568100f76e6a2ef9e
Saadraja007 <102851484+Saadraja007@users.noreply.github.com>
2024-05-13 13:14:33 +0500

    Update simple_processor.v

--- Verilog/Simple Processor/simple_processor.v
+++ Verilog/Simple Processor/simple_processor.v
@@ -1,23 +1,7 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 08/11/2023 06:39:28 PM
-// Design Name: 
-// Module Name: simple_processor
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
+// Engineer: Saad Shabbir
+
 
 
 module simple_processor(
